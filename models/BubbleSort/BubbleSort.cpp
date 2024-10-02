b1f200da9ee16c41b89679446cffa27246d15d38
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-02 03:59:48 +0300

    Update BubbleSort.cpp
    clean-up

--- models/BubbleSort/BubbleSort.cpp
+++ models/BubbleSort/BubbleSort.cpp
@@ -1,14 +1,18 @@
 #include "BubbleSort.h"
 
-template<typename T>
-BubbleSort<T>::BubbleSort(T* arr, int s) : Sort<T>(arr, s) {}
+template <typename T>
+BubbleSort<T>::BubbleSort(T *arr, int s) : Sort<T>(arr, s) {}
 
-template<typename T>
-void BubbleSort<T>::sort() {
-    for (int i = 0; i < this->size - 1; i++) {
-        for (int j = 0; j < this->size - i - 1; j++) {
-            if (this->array[j] > this->array[j + 1]) {
-                this->swap(this->array[j], this->array[j + 1]);  
+template <typename T>
+void BubbleSort<T>::sort()
+{
+    for (int i = 0; i < this->size - 1; i++)
+    {
+        for (int j = 0; j < this->size - i - 1; j++)
+        {
+            if (this->array[j] > this->array[j + 1])
+            {
+                this->swap(this->array[j], this->array[j + 1]);
             }
         }
     }
