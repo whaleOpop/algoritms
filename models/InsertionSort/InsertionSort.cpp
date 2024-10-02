8105917b7b6b4f47c833cac56799a2dc253dc0e9
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-02 04:00:56 +0300

    Update InsertionSort.cpp
    clean-up

--- models/InsertionSort/InsertionSort.cpp
+++ models/InsertionSort/InsertionSort.cpp
@@ -1,14 +1,17 @@
 #include "InsertionSort.h"
 
-template<typename T>
-InsertionSort<T>::InsertionSort(T* arr, int s) : Sort<T>(arr, s) {}
+template <typename T>
+InsertionSort<T>::InsertionSort(T *arr, int s) : Sort<T>(arr, s) {}
 
-template<typename T>
-void InsertionSort<T>::sort() {
-    for (int i = 1; i < this->size; i++) {
+template <typename T>
+void InsertionSort<T>::sort()
+{
+    for (int i = 1; i < this->size; i++)
+    {
         T key = this->array[i];
         int j = i - 1;
-        while (j >= 0 && this->array[j] > key) {
+        while (j >= 0 && this->array[j] > key)
+        {
             this->array[j + 1] = this->array[j];
             j--;
         }
