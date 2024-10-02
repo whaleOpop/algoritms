65c209eb7063a96809e7646931edf626bf846915
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-02 03:59:58 +0300

    Update ExchangeSort.cpp
    clean-up

--- models/ExchangeSort/ExchangeSort.cpp
+++ models/ExchangeSort/ExchangeSort.cpp
@@ -1,19 +1,22 @@
 #include "ExchangeSort.h"
 
-template<typename T>
-ExchangeSort<T>::ExchangeSort(T* arr, int s) : Sort<T>(arr, s) {}
+template <typename T>
+ExchangeSort<T>::ExchangeSort(T *arr, int s) : Sort<T>(arr, s) {}
 
-template<typename T>
-void ExchangeSort<T>::sort() {
-    for (int i = 0; i < this->size - 1; i++) {
-        for (int j = i + 1; j < this->size; j++) {
-            if (this->array[j] < this->array[i]) {
+template <typename T>
+void ExchangeSort<T>::sort()
+{
+    for (int i = 0; i < this->size - 1; i++)
+    {
+        for (int j = i + 1; j < this->size; j++)
+        {
+            if (this->array[j] < this->array[i])
+            {
                 this->swap(this->array[i], this->array[j]);
             }
         }
     }
 }
-
 
 template class ExchangeSort<int>;
 template class ExchangeSort<double>;
