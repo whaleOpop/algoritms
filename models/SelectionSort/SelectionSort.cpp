fc5bcb4ae028d1db30dceed8e4d7d64cbb27dd28
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-02 04:01:04 +0300

    Update SelectionSort.cpp
    clean-up

--- models/SelectionSort/SelectionSort.cpp
+++ models/SelectionSort/SelectionSort.cpp
@@ -1,18 +1,22 @@
 #include "SelectionSort.h"
 
-template<typename T>
-SelectionSort<T>::SelectionSort(T* arr, int s) : Sort<T>(arr, s) {}
+template <typename T>
+SelectionSort<T>::SelectionSort(T *arr, int s) : Sort<T>(arr, s) {}
 
-template<typename T>
-void SelectionSort<T>::sort() {
-    for (int i = 0; i < this->size - 1; i++) {
+template <typename T>
+void SelectionSort<T>::sort()
+{
+    for (int i = 0; i < this->size - 1; i++)
+    {
         int minIndex = i;
-        for (int j = i + 1; j < this->size; j++) {
-            if (this->array[j] < this->array[minIndex]) {
+        for (int j = i + 1; j < this->size; j++)
+        {
+            if (this->array[j] < this->array[minIndex])
+            {
                 minIndex = j;
             }
         }
-        this->swap(this->array[i], this->array[minIndex]);  
+        this->swap(this->array[i], this->array[minIndex]);
     }
 }
 
